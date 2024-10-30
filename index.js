299b9e872a9d2d1f4b930cd979540438c2c58fc3
AnubhavBajaj <anubhavbajaj112358@gmail.com>
2024-10-30 11:37:45 +0530

    Improving formatting

--- index.js
+++ index.js
@@ -3,44 +3,44 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-todo = [{id:"lakndflnalsdn",title:"alndlfnalsdknf"}];
+todo = [{ id: "lakndflnalsdn", title: "alndlfnalsdknf" }];
 
-app.get("/todo",(req,res)=>{
-    res.json(todo);
-});
-
-app.post("/todo",(req,res)=>{
-    const newtodo = {
-        id : crypto.randomUUID(),
-        ...req.body
-    }
-    todo.push(newtodo)
-    return res.status(201).json(newtodo);
-});
-app.put("/todo/:id",(req,res)=>{
-    let todoFindIndex = todo.findIndex(t => t.id === req.params.id);
-    if(todoFindIndex> -1){
-        todo[todoFindIndex] = {
-            id : todo[todoFindIndex].id,
-            ... req.body
-        }
-        return res.status(200).json(todo[todoFindIndex]);
-    }
-    return res.status(404).json({
-        error: "Not Found"
-    });
-});
-app.delete("/todo/:id",(req,res)=>{
-    let todoFindIndex = todo.findIndex(t => t.id === req.params.id);
-    if(todoFindIndex> -1){
-        const deletedTodo = todo.splice(todoFindIndex,1);
-        return res.status(204).json(deletedTodo);
-    }
-    return res.status(404).json({
-        error: "Not Found"
-    });
+app.get("/todo", (req, res) => {
+  res.json(todo);
 });
 
-app.listen(8080,()=>{
-    console.log('server is running on http://localhost:8080');
-})
+app.post("/todo", (req, res) => {
+  const newtodo = {
+    id: crypto.randomUUID(),
+    ...req.body,
+  };
+  todo.push(newtodo);
+  return res.status(201).json(newtodo);
+});
+app.put("/todo/:id", (req, res) => {
+  let todoFindIndex = todo.findIndex((t) => t.id === req.params.id);
+  if (todoFindIndex > -1) {
+    todo[todoFindIndex] = {
+      id: todo[todoFindIndex].id,
+      ...req.body,
+    };
+    return res.status(200).json(todo[todoFindIndex]);
+  }
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+app.delete("/todo/:id", (req, res) => {
+  let todoFindIndex = todo.findIndex((t) => t.id === req.params.id);
+  if (todoFindIndex > -1) {
+    const deletedTodo = todo.splice(todoFindIndex, 1);
+    return res.status(204).json(deletedTodo);
+  }
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+app.listen(8080, () => {
+  console.log("server is running on http://localhost:8080");
+});
